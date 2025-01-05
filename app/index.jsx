46f44294ec48dacf523a2d51d8dbd2ad5cf6bf96
Varsha <varsha@Varshas-MacBook-Air.local>
2025-01-05 12:47:01 +0530

    initialize chat app

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View className="bg-red-200 flex:1 pt-10">
+      <Text className="text-3xl text-center">Home</Text>
+    </View>
+  )
+}
+
+export default Home
